d6bf67026d6957e00ace381f2dbc5127fc0a0667
Jackson Ming Hu <huming2207@gmail.com>
2018-10-16 00:07:26 +1100

    CalLoader: initial (and partial) attempts

---
+++ main/ical/CalLoader.cpp
@@ -0,0 +1,94 @@
+#include <esp_log.h>
+#include "CalLoader.h"
+
+
+
+void CalLoader::replaceString(std::string &origString, const std::string &oldString, const std::string &newString)
+{
+    std::string::size_type pos = 0u;
+    while((pos = origString.find(oldString, pos)) != std::string::npos) {
+        origString.replace(pos, oldString.length(), newString);
+        pos += newString.length();
+    }
+}
+
+void CalLoader::parseEventFromUrl(const char *url)
+{
+    char *buffer = (char *)malloc(CONFIG_HTTP_RECV_BUFFER_SIZE + 1);
+    if (buffer == nullptr) {
+        ESP_LOGE(LOG_TAG, "Cannot malloc http receive buffer");
+        return;
+    }
+
+    // Set the configuration with Google's root certificate
+    esp_http_client_config_t config = {};
+    config.url = url;
+    config.event_handler = httpEventHandler;
+    config.cert_pem = google_root_cert_pem_start;
+
+    // Initialize and perform the request
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err;
+    if ((err = esp_http_client_open(client, 0)) != ESP_OK) {
+        ESP_LOGE(LOG_TAG, "Failed to open HTTP connection: %s", esp_err_to_name(err));
+        free(buffer);
+        return;
+    }
+
+    // There is no content length in many major calendar service provider (e.g. Google Calendar and Allocate+)
+    // so I'm not gonna use the way that the SDK example is using.
+    int readLength = 0;
+    while(readLength >= 0) {
+        readLength = esp_http_client_read(client, buffer, CONFIG_HTTP_RECV_BUFFER_SIZE);
+        ESP_LOGD(LOG_TAG, "Got data, length is %d bytes", readLength);
+    }
+
+    ESP_LOGI(LOG_TAG, "HTTP Stream reader Status = %d, content_length = %d",
+             esp_http_client_get_status_code(client),
+             esp_http_client_get_content_length(client));
+    esp_http_client_close(client);
+    esp_http_client_cleanup(client);
+    free(buffer);
+}
+
+esp_err_t CalLoader::httpEventHandler(esp_http_client_event_t *evt)
+{
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR: {
+            ESP_LOGD(LOG_TAG, "Got error!");
+            break;
+        }
+
+        case HTTP_EVENT_ON_CONNECTED: {
+            ESP_LOGD(LOG_TAG, "Connection established!");
+            break;
+        }
+
+        case HTTP_EVENT_HEADER_SENT: {
+            ESP_LOGD(LOG_TAG, "Request headers has been sent!");
+            break;
+        }
+
+        case HTTP_EVENT_ON_HEADER: {
+            ESP_LOGD(LOG_TAG, "Got response header, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        }
+
+        case HTTP_EVENT_ON_DATA: {
+            ESP_LOGD(LOG_TAG, "Got response data section, length is %d bytes", evt->data_len);
+            break;
+        }
+
+        case HTTP_EVENT_ON_FINISH: {
+            ESP_LOGD(LOG_TAG, "Data transfer finished!");
+            break;
+        }
+
+        case HTTP_EVENT_DISCONNECTED: {
+            ESP_LOGD(LOG_TAG, "Server disconnected!");
+            break;
+        }
+    }
+
+    return ESP_OK;
+}
---
+++ main/ical/CalLoader.h
@@ -0,0 +1,33 @@
+//
+// Created by hu on 15/10/18.
+//
+
+#ifndef QUESERA_CALLOADER_H
+#define QUESERA_CALLOADER_H
+
+#include <string>
+#include <esp_system.h>
+#include <esp_http_client.h>
+
+#define LOG_TAG "CalLoader"
+
+extern const char google_root_cert_pem_start[] asm("_binary_google_root_cert_pem_start");
+extern const char google_root_cert_pem_end[]   asm("_binary_google_root_cert_pem_end");
+
+class CalLoader
+{
+    public:
+        void parseEventFromUrl(const char *url);
+
+    private:
+        void replaceString(std::string &origString, const std::string &oldString, const std::string &newString);
+        static esp_err_t httpEventHandler(esp_http_client_event_t *evt);
+        time_t firstDtStart;
+        time_t firstDtEnd;
+        time_t secondDtStart;
+        time_t secondDtEnd;
+        std::string firstSummary;
+        std::string secondSummary;
+};
+
+#endif //QUESERA_CALLOADER_H
